1bd6983bcffa317fc23958106d43e133cbf8306d
OdionOseiwe <oseiweifebhor@gmail.com>
2022-09-10 23:35:12 +0100

    updated

--- pages/index.tsx
+++ pages/index.tsx
@@ -8,12 +8,68 @@ import { useAccount, useContractRead } from 'wagmi'
 
 
 const Home: NextPage = () => { 
-  const {data, isError, isloading} = useFunctionReads("started");
-  console.log(data, isError, isloading);
+  const {data:started, isError, isloading} = useFunctionReads("started");
+  console.log(started, isError, isloading);
+  const {data:ended} = useFunctionReads("ended");
   const {data:ownerAddress} = useFunctionReads("owner");
   console.log(ownerAddress, "owner address");
   const {address} = useAccount();
-  console.log(address);
+  console.log(address ,"current address");
+
+  const isAdmin = ownerAddress === address ? true : false;
+
+  const statusButton =() =>{
+    if(isloading){
+      return(
+        <div>
+          <h4>{isloading}</h4>
+        </div>
+      )
+    }
+    if (isError) {
+      return(
+        <div>
+          <h4>{isError}</h4>
+        </div>
+      )
+    }
+    if(isAdmin === true && !started){
+      return(
+        <div className='m-4'>
+          <button className='border bg-slate-500 rounded-none p-3'> start auction  
+          {/* <RightArrow />  */}
+          </button>
+        </div>
+      )
+    }
+    if (started && !ended) {
+      return(
+        <div className='m-4'>
+          <button className='border bg-slate-500 rounded-none p-3'> in progress  
+          {/* <RightArrow />  */}
+          </button>
+        </div>
+      )
+    }
+    if (!started && !isAdmin) {
+      return(
+        <div className='m-4'>
+          <button className='border bg-slate-500 rounded-none p-3'> has not started  
+          {/* <RightArrow />  */}
+          </button>
+        </div>
+      )
+    }
+    if (ended) {
+      return(
+        <div className='m-4'>
+          <button className='border bg-slate-500 rounded-none p-3'> has  ended 
+          {/* <RightArrow />  */}
+          </button>
+        </div>
+      )
+    }
+  }
   
   return (
     <div className="grid grid-cols-1 justify-between w-9/12 mx-auto lg:grid-cols-2">
@@ -21,11 +77,7 @@ const Home: NextPage = () => {
            <Image src="https://source.unsplash.com/random/?productivity,city" width={700} height={500}/>
         </div>
         <div className='flex-1 bg-slate-800 p-2 text-center ' > 
-          <div className='m-4'>
-              <button className='border bg-slate-500 rounded-none p-3'> start auction  
-              {/* <RightArrow />  */}
-              </button>
-           </div>
+          {statusButton()}
           <div className='mt-4'>
             <span>highest bid:</span>
             <span className='mx-1 text-2xl text-red-500'>3:00</span>
